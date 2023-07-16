2acaacd130f27a44fa36675cdcdda5ce944688d0
Aleksejs Grocevs <aleksejs@grocevs.pro>
2023-07-17 01:26:20 +0300

    SerenityInfoTable Explorer link added

--- src/components/SerenityInfoTable.vue
+++ src/components/SerenityInfoTable.vue
@@ -2,7 +2,22 @@
   <el-table :data="tableData">
     <el-table-column :min-width="10" prop="id" label="#"/>
     <el-table-column :min-width="35" prop="blockTime" label="Block time"/>
-    <el-table-column prop="trxId" label="Transaction Hash/ID"/>
+    <el-table-column prop="trxId" label="Transaction Hash/ID">
+      <template #default="scope">
+        <el-tooltip
+          class="box-item"
+          effect="dark"
+          content="Open transaction details in Solana Explorer"
+          raw-content
+          placement="bottom-start"
+        >
+        <a :href="'https://explorer.solana.com/tx/'+scope.row.trxId+'?cluster=devnet'" target="_blank">
+<!--            <svg-info class="icon-normal mr-extra-small" style="width: 25px;position: relative;margin: 0;top: 5px;"/>-->
+            {{ scope.row.trxId.substring(0, 35) }}...
+        </a>
+        </el-tooltip>
+      </template>
+    </el-table-column>
     <el-table-column prop="sender" label="Sender address"/>
   </el-table>
 </template>
@@ -12,6 +27,8 @@ import type {StoreType} from "@/store";
 import {useStore} from "@/store";
 import {ref} from "vue";
 import HttpResponse from "@/common/api/HttpResponse";
+// import SvgInfo from "@/assets/icons/info.svg?component";
+
 
 const store: StoreType = useStore();
 
