553b8878590d96aeb65cc429a1037ca0590f0dad
Anton Tokalov <anton@altermail.co>
2023-09-05 21:48:08 +0300

    BBLOCK-502: add unique callers table to serenity endpoint

---
+++ src/components/SerenityLatestCallsTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-table :data="tableData">
+    <el-table-column :min-width="10" prop="id" label="#"/>
+    <el-table-column :min-width="35" prop="blockTime" label="Block time"/>
+    <el-table-column prop="trxId" label="Transaction Hash/ID">
+      <template #default="scope">
+        <el-tooltip
+          class="box-item"
+          effect="dark"
+          content="Open transaction details in BSCScan"
+          raw-content
+          placement="bottom-start"
+        >
+          <el-link :href="'https://testnet.bscscan.com/tx/' + scope.row.trxId" target="_blank" :underline="false">
+            {{ scope.row.trxId.substring(0, 35) }}...
+          </el-link>
+        </el-tooltip>
+      </template>
+    </el-table-column>
+    <el-table-column prop="sender" label="Sender address"/>
+    <el-table-column prop="functionHash" label="Function Hash"/>
+  </el-table>
+</template>
+
+<script lang="ts" setup>
+defineProps(["tableData"]);
+</script>
--- src/views/SerenityInfo.vue
+++ src/views/SerenityInfo.vue
@@ -1,10 +1,61 @@
 <template>
   <PageWrapper :full-width="true" class="fs-slightly-larger">
-    <SerenityInfoTable />
+    <div class="my-small">Total successful TX count: {{ totalCount }}</div>
+    <h1>Successful transactions from unique addresses:</h1>
+    <SerenityUniqueCallersTable :table-data="uniqueCalls" />
+    <h1>100 latest successful transactions:</h1>
+    <SerenityLatestCallsTable :table-data="latestTransactions" />
   </PageWrapper>
 </template>
 
 <script setup lang="ts">
-import SerenityInfoTable from "@/components/SerenityInfoTable.vue";
+import SerenityLatestCallsTable from "@/components/SerenityLatestCallsTable.vue";
+import SerenityUniqueCallersTable from "@/components/SerenityUniqueCallersTable.vue";
 import PageWrapper from "@/components/PageWrapper.vue";
+import HttpResponse from "@/common/api/HttpResponse";
+import { StoreType, useStore } from "@/store";
+import { ref } from "vue";
+
+const store: StoreType = useStore();
+
+let latestTransactions = ref();
+let uniqueCalls = ref();
+let totalCount = ref();
+
+async function loadData() {
+  const response: Response = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/serenity/getLastInteractions`,
+    {
+      body: null,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Token": store.state.UserModule?.token || "",
+      },
+      method: "POST",
+    }
+  );
+  const responseData = await HttpResponse.fromResponse<null>(response);
+
+  const latestContractCallsResponse = responseData.payload.latestContractCalls;
+  const uniqueCallsResponse = responseData.payload.uniqueCallers;
+  totalCount.value = responseData.payload.totalCount;
+
+  let latestContractCallsRet = [];
+  for (let i = 0; i < latestContractCallsResponse.length; i++) {
+    let el = latestContractCallsResponse[i];
+    el.id = i + 1;
+    latestContractCallsRet.push(el);
+  }
+  latestTransactions.value = latestContractCallsRet;
+
+  let uniqueCallsRet = [];
+  for (let i = 0; i < uniqueCallsResponse.length; i++) {
+    let el = uniqueCallsResponse[i];
+    el.id = i + 1;
+    uniqueCallsRet.push(el);
+  }
+  uniqueCalls.value = uniqueCallsRet;
+}
+
+loadData();
 </script>
