d33e1ae98328cca9500670b93975edb54dffa9b4
Anton Tokalov <anton@altermail.co>
2023-09-25 11:31:03 +0300

    BBLOCK-524: update dtos and remove deprecated code

--- src/common/api/dto/AnnouncementsDto.ts
+++ src/common/api/dto/AnnouncementsDto.ts
@@ -2,18 +2,8 @@ export default class AnnouncementsDto {
   id: string = "";
   title: string = "";
   description: string = "";
-  image: string = "";
+  bannerUrl: string = "";
   imageType: string = "";
   socials: Array<{ type: string; link: string; title?: string }> | null = [];
   createdOn: string = "";
-  // deprecated
-  bannerImg: string = "";
-  content: string = "";
-  mainLink: string = "";
-  mainLinkTitle: string = "";
-  twitterLink: string = "";
-  telegramLink: string = "";
-  youtubeLink: string = "";
-  redditLink: string = "";
-  discordLink: string = "";
 }
